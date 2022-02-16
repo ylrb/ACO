524e3f92e52de1d1f1f971b7e9df8622275cd4d4
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-02-16 15:14:56 +0100

    init

--- Fourmi.java
+++ Fourmi.java
@@ -9,9 +9,9 @@ public class Fourmi {
     public Fourmi(int xinit, int yinit) {
         x = xinit;
         y = yinit;
-        direction = 8*(int)Math.random(); 
+        direction = 8*(int)Math.random(); // Provisoire, à discuter
         porteNourriture = false;
-        vitesse = 1.0;
+        vitesse = 1.0; // Provisoire, voir ce que ca implique si on le change
     }
 
 }
