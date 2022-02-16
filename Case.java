36d501357fef8d3fe9b3224db3d85de18b2d518b
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-02-16 14:55:27 +0100

    Debut de la classe case

---
+++ Case.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Case {
+    
+    //Coordonnées et couleur d'une case
+    protected int x;
+    protected int y;
+    protected Color couleur;
+
+    private Color getCouleur() {
+        return couleur;
+    }
+}
