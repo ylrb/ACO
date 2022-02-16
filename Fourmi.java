68e27478c4bbec9c90337a2881f9212997054d4e
YL <ngowg5p3v@relay.firefox.com>
2022-02-16 15:06:57 +0100

    fourmi

--- Fourmi.java
+++ Fourmi.java
@@ -1,6 +1,17 @@
 public class Fourmi {
 
-    public static void main (String[] args) {
-        System.out.println("Je suis fourmi.");
+    private int x;
+    private int y;
+    private int direction;
+    private boolean porteNourriture;
+    private double vitesse;
+
+    public Fourmi(int xinit, int yinit) {
+        x = xinit;
+        y = yinit;
+        direction = 8*(int)Math.random(); 
+        porteNourriture = false;
+        vitesse = 1.0;
     }
+
 }
