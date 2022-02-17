9d979a8812e3b05c766b10547b63d6b0ddc8ae8a
Lamaqua <lamaqua45682@hotmail.fr>
2022-02-17 17:32:40 +0100

    jzef

--- Carte.java
+++ Carte.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
+
 public class Carte extends JFrame implements ActionListener, MouseListener {
     private int width;
     private int height;
