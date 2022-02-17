40e9e770ae3dced39026b5bd34147d17d8d0c1a6
YL <ngowg5p3v@relay.firefox.com>
2022-02-17 15:20:16 +0100

    reset

--- Fourmi.java
+++ Fourmi.java
@@ -1,25 +1,6 @@
 public class Fourmi {
 
-    private int x;
-    private int y;
-    private int direction;
-    private boolean porteNourriture;
-    private double vitesse;
-
-    public Fourmi(int xinit, int yinit) {
-        x = xinit;
-        y = yinit;
-        direction = 8*(int)Math.random(); // Provisoire, à discuter
-        porteNourriture = false;
-        vitesse = 1.0; // Provisoire, voir ce que ca implique si on le change
-    }
-
-    public void avancer(int[][] carte, int x, int y) {
-        System.out.println("Y'a rien là");
-    }
-
-    public void deplacer(int x, int y) {
-        System.out.println("Y'a rien là");
-    }
+    private double x;
+    private double y;
 
 }
