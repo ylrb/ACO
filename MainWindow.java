21ac42c5827502605bb1eb67319f47406b4ec521
yoryd <yoryyy@live.fr>
2022-02-17 22:17:02 +0100

    ihm update

--- MainWindow.java
+++ MainWindow.java
@@ -6,21 +6,23 @@ public class MainWindow extends JFrame {
     private final int height = 700;
 
     public MainWindow() {
-        this.setBounds(100,100,1000,700);
 
-        JPanel container = new JPanel();
-        container.setLayout(null);
+        //Conteneur principale
+        JSplitPane container = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         container.setBackground(Color.black);
-        container.setSize(getWidth(), getHeight());
-        int containerHeight = this.getHeight();
-        int containerWidth = this.getWidth();
-        System.out.println(containerHeight + containerWidth);
-        this.add(container);
         
         Carte carte = new Carte();
-        carte.setBounds((int)(0.2*containerWidth), 0, (int)(0.8*containerWidth), containerHeight);
-        container.add(carte);
+        JPanel settings = new JPanel();
+        settings.setBackground(Color.red);
+        //carte.setBounds((int)(0.2*containerWidth), 0, (int)(0.8*containerWidth), containerHeight);
+        container.setRightComponent(carte);
+        container.setLeftComponent(settings);
+        //container.setDividerLocation(0.2);
+        container.setResizeWeight(0.2);
 
+        this.add(container, BorderLayout.CENTER);
+        this.setMinimumSize(new Dimension(width, height));
+        pack();
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
         repaint();
