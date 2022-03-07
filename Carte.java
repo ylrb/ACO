8465c744b23974b5799a0e78629960466a41b22a
YL <ngowg5p3v@relay.firefox.com>
2022-03-07 12:19:53 +0100

    Opti

--- Carte.java
+++ Carte.java
@@ -11,7 +11,7 @@ import java.io.File;
 public class Carte extends JPanel implements ActionListener, MouseListener {
     
     // Tous les éléments du terrain, qui sont contenus séparément dans des listes                                                                                        
-    private ArrayList<Fourmi> fourmis = new ArrayList<Fourmi>();
+    private ArrayList<Fourmi> fourmis = new ArrayList<Fourmi>(NOMBRE_FOURMIS);
     private LinkedList<Pheromone> pheromonesAller = new LinkedList<Pheromone>();
     private LinkedList<Pheromone> pheromonesRetour = new LinkedList<Pheromone>();
     private ArrayList<Nourriture> nourritures = new ArrayList<Nourriture>();
