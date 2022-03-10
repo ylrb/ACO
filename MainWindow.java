827b7ecbad3884136172c4ae269e5a4353847787
YL <ngowg5p3v@relay.firefox.com>
2022-03-10 09:28:30 +0100

    Séance 1

--- MainWindow.java
+++ MainWindow.java
@@ -10,27 +10,25 @@ public class MainWindow extends JFrame {
 
         //Conteneur principal
         JPanel conteneur = new JPanel();
-        //conteneur.setBounds(100,100,getWidth()-getInsets().left-getInsets().right,getHeight()-getInsets().top-getInsets().bottom);
+        conteneur.setBounds(100,100,getWidth()-getInsets().left-getInsets().right,getHeight()-getInsets().top-getInsets().bottom);
+        System.out.println(largeur +" "+ hauteur);
         Carte carte = new Carte();
         //Parametres parametres = new Parametres();
         JPanel parametres = new JPanel();
         parametres.setBackground(Color.red);
         parametres.setLayout(null);
-        parametres.setBounds(0,0,(int)(0.2*getWidth()), (int)getHeight());
+        parametres.setBounds(100,100,(int)(0.2*getWidth()), (int)getHeight());
 
         carte.setBounds((int)(0.2*getWidth()), 0, (int)(0.8*getWidth()), getHeight());
 
         conteneur.setLayout(null);
         conteneur.add(parametres);
         conteneur.add(carte);
-    
-        this.setResizable(false);
-        this.setContentPane(conteneur);
-        this.setPreferredSize(new Dimension(largeur, hauteur));
-        this.setResizable(false);
+        
         this.add(conteneur);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        repaint();
     }
 
 }
