ceecd549f3e4ad791b52585970dcc983456153e1
YL <ngowg5p3v@relay.firefox.com>
2022-03-25 08:33:12 +0100

    Optimisations mineures

--- MainWindow.java
+++ MainWindow.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 
--- Segment.java
+++ Segment.java
@@ -1,5 +1,3 @@
-import javax.lang.model.util.ElementScanner6;
-
 public class Segment {
     Vecteur pointA;
     Vecteur pointB;
